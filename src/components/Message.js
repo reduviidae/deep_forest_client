c37e456295bfbd9a52632b1f39e2a2d50cebfb8a
reduviidae <wiljago@gmail.com>
2019-04-15 11:19:28 -0400

    chat images broken, run away

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Message = props => {
   let options = { hour: "2-digit", minute: "2-digit", weekday: 'long', month: 'long', day: 'numeric' };
   let sentOn = new Date(props.message.created_at).toLocaleDateString("en-US", options)
+  let imgSrc = `../img/${props.message.user.avatar}.png`
   return (
     <div className="message">
+      <img className="chat-avi" src={require(imgSrc)} alt={`${props.message.user.avatar}`}/>
       <h5 className="msg-user-name">{props.message.user.name} ({props.message.user.pronouns})</h5>
       <p className="message-content">{props.message.content}</p>
       <p className="sent-on">sent on: {sentOn}</p>
@@ -16,4 +18,3 @@ export default Message;
 
 
 // removed chat avi because it was crashing Heroku
-// <img className="chat-avi" src={require(`../img/${props.message.user.avatar}.png`)} alt={`${props.message.user.avatar}`}/>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,7 @@
-export const API_ROOT = 'https://deep-forest-api.herokuapp.com/api/v1/';
-export const CABLE = 'ws://deep-forest-api-rpc.herokuapp.com/cable';
+// export const API_ROOT = 'https://deep-forest-api.herokuapp.com/api/v1/';
+// export const CABLE = 'wss://deep-forest-api-rpc.herokuapp.com/cable';
+export const API_ROOT = 'http://localhost:3000/api/v1/';
+export const CABLE = 'ws://localhost:3334/cable';
 export const AUTH_HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
@@ -16,9 +18,6 @@ export const BASIC_HEADERS = {
       Accept: 'application/json',
       Authorization: `${localStorage.token}`
     };
-
-export const PUB_KEY = `pub-c-b19b4550-28e5-4e8b-99df-2320a631698b`;
-export const SUB_KEY = `sub-c-056dc350-4b25-11e9-b0df-968893e54af3`;
 export const JWT_SECRET = `littlefatdogggies`;
 export const GAME_ID = 5;
 export const DRAWING_ID = 5;
